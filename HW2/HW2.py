14b9a43bc2122032b12d50f5be2b60d8e02a9978
Egor Loshkarev <Johnnylao@mail.ru>
2022-10-09 14:27:40 +0600

    HW2 update

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -5,40 +5,44 @@ import csv
 with open("phonebook_raw.csv", encoding="UTF_8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-# print(contacts_list)
+    new_list = []
 
 # TODO 1: выполните пункты 1-3 ДЗ
-new_list = []
-name = []
-# firstname =[]
-# surname = []
-phones = []
-email = []
 
-for q in contacts_list:
-    for w in q:
-        # print(w)
-        fio = r'([А-Я]{1}[а-яё]{1,23})\s([А-Я]{1}[а-яё]{1,23})\s?([А-Я]{1}[а-яё]{1,23})?'
-        compiled1 = re.compile(fio)
-        result = re.search(fio, str(w))
-        # print(result)
-        if result != None:
-            name.append(result)
 
-        phone = r'(\+\d|\d)\s*(\(|)(\d{3})[\s\)-]*(\d{3})\-*(\d{2})\-*(\d{2})(\s\(?доб.\s\d+)?'
-        # sub = r'+7(\3)\4-\5-\6\s\7'
-        compiled2 = re.compile(phone)
-        result2 = compiled2.match(str(w))
-        # sub_result = re.sub(phone, sub, w) ПРИ ЗАМЕНЕ ВЫДАЁТ ГОРУ ОШИБОК, ПРОБЛЕМА В ЦИКЛЕ? Как вывести текст из списка в текст?
-        # print(result2)
-        if result2 != None:
-            phones.append(result2)
+def name_add():
+    fio = r'([А-Я]{1}[а-яё]{1,23})'
+    sub = r'\1'
+    for column in contacts_list[1:]:
+        line = column[0] + column[1] + column[2]
+        if len((re.sub(fio, sub, line).split())) == 3:
+            column[0] = re.sub(fio, sub, line).split()[0]
+            column[1] = re.sub(fio, sub, line).split()[1]
+            column[2] = re.sub(fio, sub, line).split()[2]
+        elif len((re.sub(fio, sub, line).split())) == 2:
+            column[0] = re.sub(fio, sub, line).split()[0]
+            column[1] = re.sub(fio, sub, line).split()[1]
+            column[2] = ''
+        elif len((re.sub(fio, sub, line).split())) == 1:
+            column[0] = re.sub(fio, sub, line).split()[0]
+            column[1] = ''
+            column[2] = ''
 
-new_list.append(contacts_list[0])
-p_n = [zip(name, phones)] # Как увидеть в принте ZIP?
-print(p_n)
-new_list.append(p_n)
-print(new_list)
+def phone_add():
+    phone = r"(\+\d|\d)\s*(\(|)(\d{3})[\s\)-]*(\d{3})\-*(\d{2})\-*(\d{2})(\s\(?доб.\s\d+)?"
+    # sub = r'+7(\3)\4-\5-\6\s\7'
+    compiled2 = re.compile(phone)
+    result2 = compiled2.match(str(w))
+    # sub_result = re.sub(phone, sub, w) ПРИ ЗАМЕНЕ ВЫДАЁТ ГОРУ ОШИБОК, ПРОБЛЕМА В ЦИКЛЕ? Как вывести текст из списка в текст?
+    # print(result2)
+
+
+if __name__ == '__main__':
+    for contact in contacts_list:
+        if contact != new_list:
+            new_list.append(contact)
+    name_add()
+    print(new_list)
 
 
 # TODO 2: сохраните получившиеся данные в другой файл
