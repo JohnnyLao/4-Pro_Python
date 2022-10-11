4b3b9ecb8dfc2731d1fe97f23afa1856d09c2591
Egor Loshkarev <Johnnylao@mail.ru>
2022-10-11 22:16:08 +0600

    HW3 update

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,26 +1,24 @@
 import bs4
 import requests
 
-URL = 'https://habr.com/ru/'
-KEYWORDS = {'дизайн', 'фото', 'web', 'python', ''}
 
-response = requests.get(URL)
-text = response.text
-soup = bs4.BeautifulSoup(text, features='html.parser')
+def start(URL, words):
+    response = requests.get(URL)
+    text = response.text
+    soup = bs4.BeautifulSoup(text, features='html.parser')
+    articles = soup.find_all('article')
+    for article in articles:
+        preview = article.get_text().split()
+        for word in words:
+            if word in preview:
+                href = article.find('h2').find('a').attrs.get('href')
+                title = article.find("h2").find("a").find("span").text
+                date = article.find('time').attrs.get('title')
+                result = f'{date} --> {title} {URL}{href}'
+                print(result)
 
-articles = soup.find_all('article')
-for article in articles:
-    l1 = article.find(class_="tm-article-snippet")
-    for l2 in l1:
-        l3 = l2.find(class_='tm-article-body tm-article-snippet__lead')
-        print(l3)
-        for l4 in l3:
-            ptext = l3.find(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
-            res = set(l4.get_text().strip().split())
-            for word in KEYWORDS:
-                if word in ptext:
-                    href = article.find('h2').find('a').attrs.get('href')
-                    title = article.find("h2").find("a").find("span").text
-                    date = article.find('time').attrs.get('title')
-                    result = f'{date} - {title} --> {URL}{href}'
-                    print(result)
+
+if __name__ == '__main__':
+    URL = 'https://habr.com/ru/'
+    KEYWORDS = {'дизайн', 'фото', 'web', 'python','Как','как' }
+    start(URL, KEYWORDS)
